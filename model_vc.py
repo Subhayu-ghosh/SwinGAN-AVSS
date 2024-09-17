78654be596b244a494bcb780f614cb8ebdf3fe9f
SubhayuG <85089964+Subhayu-ghosh@users.noreply.github.com>
2024-09-17 15:27:46 +0530

    Update model_vc.py

--- model_vc.py
+++ model_vc.py
@@ -21,41 +21,6 @@
     _inv_mel_basis = np.linalg.pinv(audio._build_mel_basis(hparams))
     mel_basis = librosa.filters.mel(hparams.sample_rate, hparams.n_fft, n_mels=40)
     
-    class SimpleConvKAN1D(nn.Module):
-        def __init__(
-                self,
-                layer_sizes,
-                num_classes: int = 10,
-                input_channels: int = 1,
-                spline_order: int = 3,
-                groups: int = 1):
-            super(SimpleConvKAN1D, self).__init__()
-
-            kernel_size = 3  # Define kernel_size here
-
-            self.layers = nn.Sequential(
-                KANConv1DLayer(input_channels, layer_sizes[0], spline_order, kernel_size, groups=1, padding=1, stride=1,
-                            dilation=1),
-                KANConv1DLayer(layer_sizes[0], layer_sizes[1], spline_order, kernel_size, groups=groups, padding=1,
-                            stride=2, dilation=1),
-                KANConv1DLayer(layer_sizes[1], layer_sizes[2], spline_order, kernel_size, groups=groups, padding=1,
-                            stride=2, dilation=1),
-                KANConv1DLayer(layer_sizes[2], layer_sizes[3], spline_order, kernel_size, groups=groups, padding=1,
-                            stride=1, dilation=1),
-                nn.AdaptiveAvgPool1d((1,))
-            )
-
-            self.output = nn.Linear(layer_sizes[3], num_classes)
-
-            self.drop = nn.Dropout(p=0.25)
-
-        def forward(self, x):
-            x = self.layers(x)
-            x = torch.flatten(x, 1)
-            x = self.drop(x)
-            x = self.output(x)
-            return x
-
 
     class LinearNorm(torch.nn.Module):
         def __init__(self, in_dim, out_dim, bias=True, w_init_gain='linear'):
